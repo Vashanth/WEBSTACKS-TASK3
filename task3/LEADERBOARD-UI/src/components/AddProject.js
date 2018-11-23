a56316179dc2069a9ca3321ba8561d0b0ba93e01
Vashanth <43083250+Vashanth@users.noreply.github.com>
2018-11-23 19:48:02 +0530

    Update AddProject.js

--- task3/LEADERBOARD-UI/src/components/AddProject.js
+++ task3/LEADERBOARD-UI/src/components/AddProject.js
@@ -1,37 +1,37 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
  
-// class AddProject extends Component {
-//   static defaultProps={
-//     categories:['vash','venkzz','ragho']
-//   }
-//   myfunc()
-//   {
-//     console.log('VASH');
-//   }
-// 	render() {
-//     let categoryoptions=this.props.categories.map(y=>{
-//       return (<option key={y} value="category">{y}</option>)
-//     });
+class AddProject extends Component {
+  static defaultProps={
+    categories:['vash','venkzz','ragho']
+  }
+  myfunc()
+  {
+    console.log('VASH');
+  }
+	render() {
+    let categoryoptions=this.props.categories.map(y=>{
+      return (<option key={y} value="category">{y}</option>)
+    });
   
-//     return (
-//       <div>
-//       <h3> AddProject</h3>
-//       <form onSubmit={this.myfunc}>
-//       <div>
-//       <label>TITLE:</label><br />
-//       <input type="text" ref="title" />
-//             </div>
-//       <div>
-//       <label>TITLE:</label>
-//         <select ref="category">
-//         {categoryoptions}
-//         </select>
-//               </div>
-//               <input type="submit" value="sub" />
-//       </form>
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div>
+      <h3> AddProject</h3>
+      <form onSubmit={this.myfunc}>
+      <div>
+      <label>TITLE:</label><br />
+      <input type="text" ref="title" />
+            </div>
+      <div>
+      <label>TITLE:</label>
+        <select ref="category">
+        {categoryoptions}
+        </select>
+              </div>
+              <input type="submit" value="sub" />
+      </form>
+      </div>
+    );
+  }
+}
 
-// export default AddProject;
+export default AddProject;
